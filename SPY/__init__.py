c90101710ff3737e40816823988e4ecf058cc733
RyomenSukuna53 <sufyan532011@gmail.com>
2025-03-24 12:41:44 +0530

    Update __init__.py

--- SPY/__init__.py
+++ SPY/__init__.py
@@ -1,11 +1,29 @@
+import sys
 import os
-from pyrogram import Client
-
+import logging
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.handlers import MessageHandler, CallbackQueryHandler
+from os import getenv
 
 API_ID = int(os.getenv("API_ID")) 
 API_HASH = os.getenv("API_HASH") 
-TOKEN = os.getenv("TOKEN") 
+TOKEN = os.getenv("TOKEN ")
 BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID")) 
 
-bot = Client(api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN) 
+if not (API_ID and API_HASH and TOKEN):
+    sys.exit("API_ID, API_HASH, and TOKEN must be set in environment variables.")
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
+                                                    logging.StreamHandler()], format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+LOGGER = logging.getLogger(__name__)
+
+# Pyrogram Client Setup
+bot = Client(
+    "WSPY_BOT",  # Name of the session
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+    plugins=dict(root="SPY")  # Specify the plugin directory here
+)
