5bf2859c976216903cbd2813e886fe714f8e555b
RyomenSukuna53 <sufyan532011@gmail.com>
2025-03-24 14:12:40 +0530

    Create fruits.py

---
+++ SPY/modules/fruits.py
@@ -0,0 +1,7 @@
+from SPY.db import user_col, ban_col, active_game_col, 
+from SPY import bot
+from pyrogram import inlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
+fruits = ["Banana", "Apple", "Mango", "Orange", "WaterMelon"]
+
+
