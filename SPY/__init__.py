ac2d31cc18c1688f3adb7d4364398d2665f494dc
RyomenSukuna53 <sufyan532011@gmail.com>
2025-03-24 12:38:12 +0530

    Update __init__.py

--- SPY/__init__.py
+++ SPY/__init__.py
@@ -1,1 +1,11 @@
+import os
+from pyrogram import Client
+
+
+API_ID = int(os.getenv("API_ID")) 
+API_HASH = os.getenv("API_HASH") 
+TOKEN = os.getenv("TOKEN") 
+BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID")) 
+
+bot = Client(api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN) 
 
