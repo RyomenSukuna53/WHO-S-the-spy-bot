192db65d1efe5bfe7bd6e2a67c515e477d3296fa
RyomenSukuna53 <sufyan532011@gmail.com>
2025-03-24 15:13:35 +0530

    Update __main__.py

--- SPY/__main__.py
+++ SPY/__main__.py
@@ -5,4 +5,4 @@ if __name__=="__main__":
   print("Bot Started Successful🏆💪") 
   bot.run() 
   with bot:
-    bot.send_message(chat_id=-1002304401488, "MAIN WAPAS AA GYA TU FIRSE DAREGA @Uzumaki_X_Naruto_6") 
+    bot.send_message(chat_id=-1002304401488, text="MAIN WAPAS AA GYA TU FIRSE DAREGA @Uzumaki_X_Naruto_6") 
