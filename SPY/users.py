e4b231ac760d656d7a24f388004b5c1145e264d7
RyomenSukuna53 <sufyan532011@gmail.com>
2025-03-24 13:47:59 +0530

    Create users.py

---
+++ SPY/users.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from FB.db import users_col
+import math
+
+# Helper Functions
+def add_user(user_id, username):
+    """
+    Add a new user to the database with initial stats if they don't already exist.
+    """
+    if not users_col.find_one({"user_id": user_id}):
+        users_col.insert_one({
+            "user_id": user_id,
+            "firstname": first_name,
+            "username": username,
+            "start_date": datetime.now().strftime("%d-%m-%Y")
+        })
