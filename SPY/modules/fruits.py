a3353a6704475e56b0208c53e90df5014fc22044
RyomenSukuna53 <sufyan532011@gmail.com>
2025-03-28 10:49:19 +0530

    Update fruits.py

--- SPY/modules/fruits.py
+++ SPY/modules/fruits.py
@@ -1,4 +1,4 @@
-from SPY.db import user_col, ban_col, active_game_col
+from SPY.db import users_col, ban_col, active_game_col
 from SPY import bot
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
