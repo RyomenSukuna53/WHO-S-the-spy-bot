0617c9217ff83096367a670f1927f931484deb4a
RyomenSukuna53 <sufyan532011@gmail.com>
2025-04-03 14:58:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from os import getenv
 
-API_ID = int(getenv("API_ID", "18990697"))
-API_HASH = getenv("API_HASH", "f4815b9a16cb03c2f5eabe8db1cb0903")
-TOKEN = getenv("TOKEN", "7979148246:AAF1QDrslVCsOXWoGy9-XMbr05Ya9In-X1E")
+API_ID = int(getenv("API_ID", "27548865"))
+API_HASH = getenv("API_HASH", "db07e06a5eb288c706d4df697b71ab61")
+TOKEN = getenv("TOKEN", "7769196646:AAGeRD7W0q4KM-c5Ud9QVIKhXKeNdu8chxY")
 BOT_OWNER_ID = list(map(int, getenv("OWNER", "6239769036").split(',')))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Dragon_ballsopport_Xprobot") 
