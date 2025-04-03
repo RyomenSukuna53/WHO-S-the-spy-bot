276454c4e1f78a45b259a420492f78b9b1faf474
RyomenSukuna53 <sufyan532011@gmail.com>
2025-04-03 13:37:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "18990697"))
 API_HASH = getenv("API_HASH", "f4815b9a16cb03c2f5eabe8db1cb0903")
-TOKEN = getenv("TOKEN", "7506827691:AAGRtD6TrW3vDopi_-UnVETI9J5DcoS7nTU")
+TOKEN = getenv("TOKEN", "7979148246:AAF1QDrslVCsOXWoGy9-XMbr05Ya9In-X1E")
 BOT_OWNER_ID = list(map(int, getenv("OWNER", "6239769036").split(',')))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Dragon_ballsopport_Xprobot") 
